e2688a9875f41611241cbe678aad3c99826bb742
Daniel Fone <daniel@fone.net.nz>
2013-01-16 11:06:09 +1300

    Fix csv proxy

--- lib/as_csv.rb
+++ lib/as_csv.rb
@@ -1,6 +1,9 @@
-require "as_csv/csv_proxy"
 require "as_csv/version"
+require "as_csv/csv_proxy"
 require "as_csv/csv_builder"
 require "as_csv/core_ext/array"
 require "as_csv/active_model"
 require "as_csv/action_renderer"
+
+module AsCSV
+end
