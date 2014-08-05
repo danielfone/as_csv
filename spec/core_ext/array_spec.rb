6e99e1fdfa5d88b3ba88150bf0a9dd3f4c0c8ead
Daniel Fone <daniel@fone.net.nz>
2014-08-06 11:39:09 +1200

    Fix array spec

--- spec/core_ext/array_spec.rb
+++ spec/core_ext/array_spec.rb
@@ -1,28 +1,6 @@
 require 'spec_helper'
 
 describe Array do
-  it { should respond_to(:to_csv) }
-  it { should respond_to(:to_csv_with_builder) }
-  it { should respond_to(:to_csv_without_builder) }
-
-  context 'with objects responding to `as_csv`' do
-    before { subject << stub(:foo, :as_csv => {:header => 'value'})}
-
-    specify do
-      AsCSV::CSVBuilder.any_instance.should_receive(:to_csv).and_call_original
-      subject.should_not_receive(:to_csv_without_builder)
-      subject.to_csv
-    end
-  end
-
-  context 'without objects responding to `as_csv`' do
-    before { subject << 1 }
-
-    specify do
-      AsCSV::CSVBuilder.any_instance.should_receive(:to_csv).and_call_original
-      subject.should_receive(:to_csv_without_builder)
-      subject.to_csv
-    end
-  end
-
+  # In place while we undo the monkey-patching shame
+  it { should_not respond_to(:to_csv) }
 end
