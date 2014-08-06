fc12de78070a563464a6957965542cdacc268dae
Daniel Fone <daniel@fone.net.nz>
2014-08-06 13:25:34 +1200

    Move into railtie

--- lib/as_csv/core_ext/array.rb
+++ lib/as_csv/core_ext/array.rb
@@ -1,15 +1,23 @@
 require 'csv'
-require "as_csv/csv_builder"
+require 'as_csv/csv_builder'
 
 class Array
+
   def to_csv_with_builder(opts={})
-    if any? { |e| e.respond_to? :as_csv }
-      AsCSV::CSVBuilder.new(self, opts).to_csv
-    else
-      to_csv_without_builder
-    end
+    use_csv_builder? and csv_builder(opts).to_csv or to_csv_without_builder
   end
+
   alias_method :to_csv_without_builder, :to_csv
   alias_method :to_csv, :to_csv_with_builder
+
+private
+
+  def use_csv_builder?
+    any? { |e| e.respond_to? :as_csv }
+  end
+
+  def csv_builder(opts)
+    AsCSV::CSVBuilder.new self, opts
+  end
 
 end
--- lib/as_csv/csv_builder.rb
+++ lib/as_csv/csv_builder.rb
@@ -1,4 +1,4 @@
-require "csv"
+require 'csv'
 
 module AsCSV
   class CSVBuilder
